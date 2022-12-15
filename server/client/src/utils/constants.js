20f23404ab855df3dcd8057759019c90bcbb4389
HadesHappy <happyhades123@gmail.com>
2022-12-15 08:38:03 -0500

    package

--- server/client/src/utils/constants.js
+++ server/client/src/utils/constants.js
@@ -1,5 +1,5 @@
-// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/'
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://172.93.193.115:8080'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/'
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://172.93.193.115:8080'
 const CHEATNUM = 16
 export {
   BACKEND_URL,
